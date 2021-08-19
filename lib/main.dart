f4bf2a08e837056e051638fad812080373fa0b74
Duncan Schriel <duncan@merkmeeter.nl>
2021-08-19 09:44:13 +0200

    Initial setup world=time

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sandbox_flutter_routing/pages/home.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Home(),
     ));
